2266910bd4955995e6860d7aa6663ff377b608b0
Yann Raude <yann.raude@gmail.com>
2022-03-28 18:56:14 +0200

    navigation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,8 @@
 // This is where it all goes :)
+window.onload = function() {
+    menuToggle = document.getElementById( 'check' );
+
+    menuToggle.addEventListener ('click', function() {
+        document.getElementById( 'bigNav' ).classList.toggle( 'displayed' );
+    });
+};
