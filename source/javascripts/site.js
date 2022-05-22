0532f664677bdfedb7329210e53c2ee4add4e3fd
Yann Raude <yann.raude@gmail.com>
2022-05-22 19:02:50 +0200

    update css

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,8 +1,24 @@
 // This is where it all goes :)
+posAndresize = function (elt) {
+    offsets=document.getElementById(elt).getBoundingClientRect();
+    bg=document.getElementById('bg'+elt);
+    bg.style.height=(offsets.height*1.6)+"px";
+    bg.style.top=(offsets.top*.7+window.scrollY)+"px";
+}
+
 window.onload = function() {
     menuToggle = document.getElementById( 'check' );
 
     menuToggle.addEventListener ('click', function() {
         document.getElementById( 'bigNav' ).classList.toggle( 'displayed' );
     });
+
+    //posAndresize('contexte');
+
+    //bgcontexte=document.getElementById('contexte');
 };
+/*
+window.onresize = function() {
+    posAndresize('contexte');
+}
+*/
