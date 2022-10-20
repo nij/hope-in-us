a2909f9d6c01935e6253b59804f1d254d738a00c
Yann Raude <yann.raude@gmail.com>
2022-10-20 09:17:58 +0200

    tags

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,7 +4,25 @@ posAndresize = function (elt) {
     bg=document.getElementById('bg'+elt);
     bg.style.height=(offsets.height*1.6)+"px";
     bg.style.top=(offsets.top*.7+window.scrollY)+"px";
+};
+resetTools = function () {
+    tclist = document.getElementById('toolscontrol');
+    itlist = document.getElementById('toolslist');
+    for (let i = 0; i < tclist.children.length; i++) {
+        if(tclist.children[i].classList.contains('reveal')) tclist.children[i].classList.remove("reveal");
+    }
+    for (let i = 0; i < itlist.children.length; i++) {
+        if(itlist.children[i].classList.contains('reveal')) itlist.children[i].classList.remove("reveal");
+    }
 }
+revealTools = function (tag) {
+    resetTools ();
+    relist = document.getElementsByClassName(tag);
+    for (let i = 0; i < relist.length; i++) {
+        relist[i].classList.add('reveal');
+    }
+}
+
 
 window.onload = function() {
     menuToggle = document.getElementById( 'check' );
@@ -16,7 +34,19 @@ window.onload = function() {
     //posAndresize('contexte');
 
     //bgcontexte=document.getElementById('contexte');
+    tclist = document.getElementById('toolscontrol');
+    for (let i = 0; i < tclist.children.length; i++) {
+        tclist.children[i].addEventListener ('click', function() {
+            if(!tclist.children[i].classList.contains('reveal')) {
+                revealTools(tclist.children[i].classList);
+            }
+            else {
+                resetTools();
+            }
+        });
+    }
 };
+
 /*
 window.onresize = function() {
     posAndresize('contexte');
